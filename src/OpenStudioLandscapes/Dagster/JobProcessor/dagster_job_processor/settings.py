3204d036f22073676d1ad753dd893efe26de78a5
Michael Mussato <michimussato@gmail.com>
2026-01-24 16:28:05 +0100

    - change namespaces for dist

--- src/OpenStudioLandscapes/Dagster/JobProcessor/dagster_job_processor/settings.py
+++ src/OpenStudioLandscapes/Dagster/JobProcessor/dagster_job_processor/settings.py
@@ -15,8 +15,8 @@ JSON_INDENT = 2
 PADDING = 4
 SUBMISSION_JSON = "submission.json"
 OUTPUT_ROOT = pathlib.Path("/nfs/AWSPortalRoot1/out")
-INPUT_ROOT = pathlib.Path(os.environ.get("DAGSTER_JOBS_IN", "/nfs/in"))
-INPUT_ROOT_PROCESSED = pathlib.Path(os.environ.get("DAGSTER_JOBS_IN", "/nfs/in")).joinpath(".processing")  # pathlib.Path(os.getenv("DAGSTER_JOBS_IN", "/nfs/in/")) / ".processing"
+INPUT_ROOT = pathlib.Path(os.environ.get("DAGSTER_JOBS_IN", "/data/share/nfs/in"))
+INPUT_ROOT_PROCESSED = pathlib.Path(os.environ.get("DAGSTER_JOBS_IN", "/data/share/nfs/in")).joinpath(".processing")  # pathlib.Path(os.getenv("DAGSTER_JOBS_IN", "/nfs/in/")) / ".processing"
 
 DEADLINE_ERRORS = [
     # Connecting to the correct repository?
