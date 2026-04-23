aad89f33b0d5654fc4aa849b5ad79da41ef9fcbc
Michael Mussato <michimussato@gmail.com>
2026-04-23 17:51:08 +0200

    add houdini

--- src/OpenStudioLandscapes/DagsterCodeLocation/JobProcessor/deadline_templates/plugins/houdini/plugin_houdini__19_5_805.py
+++ src/OpenStudioLandscapes/DagsterCodeLocation/JobProcessor/deadline_templates/plugins/houdini/plugin_houdini__19_5_805.py
@@ -10,7 +10,11 @@ from OpenStudioLandscapes.DagsterCodeLocation.JobProcessor.deadline_templates.pl
 class PluginHoudini_19_5_805(PluginHoudiniBase):
     plugin_type: Literal['PluginHoudini_19_5_805']
 
-    executable: pathlib.Path = REZ_PACKAGES /  "houdini" / "19.5.805" / "hython"
+    executable: pathlib.Path = REZ_PACKAGES /  "houdini" / "19.5.805" / "hrender"
+
+    # Farm command:
+    # Executable=/nfs/rez-packages/wrappers/hython-19.5.805
+    # Arguments="/nfs/deadline/DeadlineRepository10/plugins/Houdini/hrender_dl.py -e -f <STARTFRAME> <ENDFRAME> 1 -d /stage/usdrender_rop_camera2 -o <QUOTE>/nfs/AWSPortalRoot1/out/Sandbox/Shot/SQ010_SH020/Layout/018/4_0997-1024_4/vivi_025.\$F4.exr<QUOTE> <QUOTE>/nfs/AWSPortalRoot1/fixtures/houdini/project/vivi_025.hip<QUOTE>"
 
     rop: str = Field(
         default="",
