514acc2c63face252172767895c390bfa1907dce
Michael Mussato <michimussato@gmail.com>
2026-05-10 12:55:12 +0200

    yaml cleanup only after the full job was processed

--- src/OpenStudioLandscapes/DagsterCodeLocation/JobProcessor/dagster_job_processor/jobs/materialize_render_job.py
+++ src/OpenStudioLandscapes/DagsterCodeLocation/JobProcessor/dagster_job_processor/jobs/materialize_render_job.py
@@ -1,8 +1,16 @@
-from dagster import AssetSelection, define_asset_job
+from dagster import (
+    AssetSelection,
+    define_asset_job,
+    AssetKey,
+)
+
+from OpenStudioLandscapes.DagsterCodeLocation.JobProcessor.dagster_job_processor.assets.read_yaml import ASSET_HEADER_JOB_PROCESSOR
 
 ingest_yaml_job = define_asset_job(
     name="ingest_yaml_job",
     selection=AssetSelection.all(
         include_sources=False,
+    ) - AssetSelection.assets(
+        AssetKey([*ASSET_HEADER_JOB_PROCESSOR["key_prefix"], "archive_job_yaml"]),
     )
 )
